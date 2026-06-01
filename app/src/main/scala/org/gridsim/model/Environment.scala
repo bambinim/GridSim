3f32927cca02d910f4b8b32b958f42d80c199614
Enrico Marchionni <enrico.marchionni@studio.unibo.it>
2026-06-01 23:45:58 +0200

    feat: update Environment trait

--- app/src/main/scala/org/gridsim/model/Environment.scala
+++ app/src/main/scala/org/gridsim/model/Environment.scala
@@ -1,16 +1,17 @@
 package org.gridsim.model
 
 import org.gridsim.common.Units.Tick.Tick
-import org.gridsim.common.Units.Power.Power
+import org.gridsim.common.Units.Power
 import org.gridsim.common.Units.GeographicPoint
 
 // TODO: to verify if ok
 case class WeatherConditions(
   irradiance: Power,  // kW/m²
-  windSpeed: Double,  // m/s
+  // windSpeed: Double,  // m/s
   temperature: Double // °C
 )
 
 trait Environment:
   def tick: Tick
-  def irradiance(point: GeographicPoint): Power
+  def irradiance(point: GeographicPoint): WeatherConditions
+  def update(): Unit
