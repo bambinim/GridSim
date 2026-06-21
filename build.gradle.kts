9bd78e381529a50f9831b453f749c970d43b7097
Matteo Bambini <matteo.bambini00@gmail.com>
2026-06-21 16:48:49 +0200

    build: fixed issue with Metals bloop in build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,3 @@ plugins {
     id("com.dorongold.task-tree") version "4.0.1"
 }
 
-buildscript {
-    repositories {
-        mavenCentral()
-    }
-    dependencies {
-        classpath("ch.epfl.scala:gradle-bloop_2.12:1.6.4")
-    }
-}
-
-allprojects {
-    apply(plugin = "bloop")
-}
