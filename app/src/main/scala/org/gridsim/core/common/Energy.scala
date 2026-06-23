83006cd27c98d60671434135692d9dbe9e045787
Matteo Bambini <matteo.bambini00@gmail.com>
2026-06-24 00:32:01 +0200

    feature(power, energy): implement extension .kw and .kwh also for integer type

--- app/src/main/scala/org/gridsim/core/common/Energy.scala
+++ app/src/main/scala/org/gridsim/core/common/Energy.scala
@@ -15,7 +15,8 @@ object Energy:
   def apply(v: Double): Energy = v
   val Zero: Energy = 0.0
 
-  given CommutativeMonoid[Energy] = cats.instances.double.catsKernelStdGroupForDouble
+  given CommutativeMonoid[Energy] =
+    cats.instances.double.catsKernelStdGroupForDouble
   given Order[Energy] = cats.instances.double.catsKernelStdOrderForDouble
   given showEnergy: Show[Energy] = Show.show(e => s"${e.show2} kWh")
 
@@ -26,8 +27,12 @@ object Energy:
     @targetName("energyUnaryMinus") def unary_- : Energy = -e
     @targetName("energyTimes") def *(scalar: Double): Energy = e * scalar
     @targetName("energyDiv") def /(o: Energy): Double = e / o
-    @targetName("energyDivScalar") def /(scalar: Double): Power = Power(e / scalar)
-    def toPower(using tick: FiniteDuration): Power = Power(e / tick.toUnit(TimeUnit.HOURS))
+    @targetName("energyDivScalar") def /(scalar: Double): Power = Power(
+      e / scalar
+    )
+    def toPower(using tick: FiniteDuration): Power = Power(
+      e / tick.toUnit(TimeUnit.HOURS)
+    )
     def min(o: Energy): Energy = if e <= o then e else o
     def max(o: Energy): Energy = if e >= o then e else o
     def abs: Energy = Math.abs(e.toDouble).kwh
@@ -36,5 +41,6 @@ object Energy:
       else if e < 0.0 then Flow.Deficit(e.abs)
       else Flow.Balanced
 
-extension (d: Double)
-  def kwh: Energy = Energy(d)
+extension (d: Double) def kwh: Energy = Energy(d)
+
+extension (i: Integer) def kwh: Energy = Energy(i.toDouble)
